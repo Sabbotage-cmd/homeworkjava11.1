33189f20ad2012d301615dd20238d281378b5e4e
Vladimir Mednis <mednisv@yandex.ru>
2020-12-07 19:32:16 +0300

    add code

--- cart-with-mocks/src/main/java/ru/netology/domain/PurchaseItem.java
+++ cart-with-mocks/src/main/java/ru/netology/domain/PurchaseItem.java
@@ -7,8 +7,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class PosterItem {
+public class PurchaseItem {
   private int id;
   private int productId;
   private String productName;
+  private int productPrice;
+  private int count;
 }
