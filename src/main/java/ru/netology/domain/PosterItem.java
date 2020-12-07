331ce196635dadc96dd3f619917fc856794ce01a
Vladimir Mednis <mednisv@yandex.ru>
2020-12-07 19:29:16 +0300

    v.1 of project

---
+++ src/main/java/ru/netology/domain/PosterItem.java
@@ -0,0 +1,14 @@
+package ru.netology.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class PosterItem {
+  private int id;
+  private int productId;
+  private String productName;
+}
